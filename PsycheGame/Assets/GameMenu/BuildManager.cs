d77ef970358b84bde43bcfdb5bc279468448acf1
Forrest Cranmer <forrest.cranmer@proton.me>
2024-10-16 15:35:23 -0500

    Added assets to game menu. Need to refactor menu to be reusable for multiple items.

--- PsycheGame/Assets/GameMenu/BuildManager.cs
+++ PsycheGame/Assets/GameMenu/BuildManager.cs
@@ -4,24 +4,24 @@ using TMPro;
 
 public class BuildManager : MonoBehaviour
 {
-    public GameObject circlePrefab;   
-    public GameObject squarePrefab;
+    public GameObject item1Prefab;   
+    public GameObject item2Prefab;
     public GameObject spawnPoint;
 
     public void Start(){
         spawnPoint = GameObject.Find("SpawnArea");
     }
 
-    public void SpawnCircle()
+    public void SpawnItem1()
     {
-        Debug.Log("Spawn Circle");
-        SpawnShape(circlePrefab);
+        Debug.Log("Spawn Item 1");
+        SpawnShape(item1Prefab);
     }
 
-    public void SpawnSquare()
+    public void SpawnItem2()
     {
-        Debug.Log("Spawn Square");
-        SpawnShape(squarePrefab);
+        Debug.Log("Spawn Item 2");
+        SpawnShape(item2Prefab);
     }
 
     void SpawnShape(GameObject shapePrefab)
@@ -34,7 +34,7 @@ public class BuildManager : MonoBehaviour
         GameObject shape = Instantiate(shapePrefab, spawnPoint.transform);
         shape.transform.localPosition = new Vector3(0, 0, 0); 
         shape.transform.localScale = Vector3.one;
-        
+
         Debug.Log($"Spawned shape: {shape.name} at position {shape.transform.localPosition}");
     }
 }
