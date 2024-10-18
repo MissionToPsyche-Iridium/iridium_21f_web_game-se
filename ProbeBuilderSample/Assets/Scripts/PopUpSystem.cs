ee2a282207b74ce66a8848de2e009fdd5fa9c98c
hkmargolis <hmargol1@asu.edu>
2024-10-18 11:57:28 -0700

    adds custom sprites and mockup in unity of probe builder

---
+++ ProbeBuilderSample/Assets/Scripts/PopUpSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+// Tutorial: https://www.youtube.com/watch?v=VaDhk2eOQXM
+public class PopUpSystem : MonoBehaviour
+{
+    public GameObject popUpBox;
+    public Animator animator;
+    public TMP_Text popUpText;
+
+    public void PopUp(string text) {
+        popUpBox.SetActive(true);
+        popUpText.text = text;
+        animator.SetTrigger("pop"); 
+
+    }
+
+ 
+}
