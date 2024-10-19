192733bf8ebc85d4f6bb171f7dca31a03a8a85d3
Shawn Chen <115514754+schen337-asu@users.noreply.github.com>
2024-10-18 21:51:54 -0700

    addressing square (on TransparentFx) layer from moving

--- PsycheGame/Assets/model-dev/MouseController.cs
+++ PsycheGame/Assets/model-dev/MouseController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class MouseController : MonoBehaviour
@@ -15,12 +18,15 @@ public class MouseController : MonoBehaviour
     }
     void Start()
     {
-        Screen.SetResolution(1920, 1080, true);
+        Screen.SetResolution(2560, 1440, true);
     }
- 
+
     void OnMouseDown()
     {
-        offset = transform.position - MouseWorldPosition();
+        if (gameObject.layer == 8)
+        {
+            offset = transform.position - MouseWorldPosition();
+        }
     }
 
     void OnMouseDrag()
@@ -31,21 +37,36 @@ public class MouseController : MonoBehaviour
         }
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == chassisTag)
+        {
+            transform.position = collision.transform.position + new Vector3(0, 0, -0.01f);
+        }
+    }
+
     void OnMouseUp()
     {
-        col.enabled = false;
-        var rayOrigin = Camera.main.transform.position;
-        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
-        RaycastHit2D hit;
-        
-        if (hit = Physics2D.Raycast(rayOrigin, rayDirection))
+        if (gameObject.layer == 8)
         {
-            if (hit.transform.tag == chassisTag)
+
+            col.enabled = false;
+            var rayOrigin = Camera.main.transform.position;
+            var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
+            RaycastHit2D hit;
+            // String pattern = "(Square)(.*)";
+
+            if (hit = Physics2D.Raycast(rayOrigin, rayDirection))
             {
-                transform.position = hit.transform.position + new Vector3(0, 0, -0.01f);
+                if (hit.transform.tag == chassisTag)
+                {
+                    transform.position = hit.transform.position + new Vector3(0, 0, -0.01f);
+                    Debug.Log("collider name: " + hit.transform.name);
+                }
             }
+            col.enabled = true;
+
         }
-        col.enabled = true;
     }
 
     Vector3 MouseWorldPosition()
@@ -53,6 +74,6 @@ public class MouseController : MonoBehaviour
         var mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
         return Camera.main.ScreenToWorldPoint(mouseScreenPos);
-    } 
+    }
 
 }
