4ff44e2ca575caa9f0a492d1f27cf8e4116e5508
hkmargolis <hmargol1@asu.edu>
2024-10-23 19:13:57 -0700

    Adds undo, undo all and scale prefab

--- PsycheGame/Assets/Scripts/BuildManger/BuildManager.cs
+++ PsycheGame/Assets/Scripts/BuildManger/BuildManager.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine.SceneManagement; 
 
 using Unity.VisualScripting;
+using System.Collections;
 
 public class BuildManager : MonoBehaviour
 {
@@ -17,12 +18,17 @@ public class BuildManager : MonoBehaviour
 
     public GameObject spawnPoint;
 
+    public Stack spawnedPartsStack;
+    [SerializeField] private int probePartScale;
+
+
     public void Start(){
         CreateInventoryButtons();
         // get container manager component attached to the BuildManager GameObject
         ContainerManager containerManager = GameObject.Find("ContainerManager").GetComponent<ContainerManager>();
 
         spawnPoint = GameObject.Find("SpawnArea");
+        spawnedPartsStack = new Stack();
         Debug.Log($"Build Manager Initialized");
     }
 
@@ -55,26 +61,46 @@ public class BuildManager : MonoBehaviour
         // }
 
         GameObject shape = Instantiate(shapePrefab, spawnPoint.transform);
-        shape.transform.localPosition = new Vector3(0, 0, 0); 
-        shape.transform.localScale = Vector3.one;
+        shape.transform.localPosition = new Vector3(500, 200, 0);
+        //shape.transform.localScale = Vector3.one;
+        shape.transform.localScale = new Vector3(probePartScale, probePartScale, 0);
+        shape.tag = "Part"; //used for UndoAllOperation()
+        spawnedPartsStack.Push(shape); //used for UndoOperation()
 
         Debug.Log($"Spawned shape: {shape.name} at position {shape.transform.localPosition}");
     }
 
     public void ExitProbeBuilder(){
         Debug.Log("Returning home!");
-        SceneManager.LoadScene("MainMenu");
+        SaveProbe(); //saves before exiting
+        SceneManager.LoadScene("MainMenu"); //returns user to main menu
     }
 
     public void SaveProbe(){
         Debug.Log("Saving probe!");
+        //saves all currently spawned and snapped probe parts to a persistant game object
+    }
+
+   
+    public void UndoAllOperation(){
+        //Destroys all spawned probe parts (snapped or not)
+        Debug.Log("UndoAll operation");
+        GameObject[] parts = GameObject.FindGameObjectsWithTag("Part");
+        foreach(GameObject part in parts) {
+        GameObject.Destroy(part);
+       }
     }
 
     public void UndoOperation(){
+        //Destroys last spawned probe part 
         Debug.Log("Undo operation");
+        //if the last user action was to spawn a part...
+        GameObject lastSpawned = (GameObject) spawnedPartsStack.Pop();
+        GameObject.Destroy(lastSpawned);
     }
 
      public void RedoOperation(){
+        //Spawns a probe part that was just destroyed?
         Debug.Log("Redo operation");
     }
 }
