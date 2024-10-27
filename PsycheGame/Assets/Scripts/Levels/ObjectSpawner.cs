76ed1d4cf4ba9c4341786b3a18d76c5d66efb74d
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-10-27 15:10:21 -0400

    add generic framework for spawning objects within a radius

---
+++ PsycheGame/Assets/Scripts/Levels/ObjectSpawner.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+    [SerializeField] private Spawnable objectToSpawn;
+    [SerializeField] private GameObject boundingArea;
+    [SerializeField] private int spawnInterval = 1;
+    [SerializeField, Min(0)] private int objectLimit = 200;
+    [SerializeField] private int initialPopulation = 0;
+
+    [Header("Spawned Object Properties")]
+    [SerializeField] private float scaleMin = 1f;
+    [SerializeField] private float scaleMax = 5f;
+    [SerializeField, Min(0f)] private float velocityMax = 20f;
+    [SerializeField, Min(0f)] private float velocityMin = 1f;
+
+    [Header("Spawnning Area")]
+    [SerializeField] private bool showRadiusInEditor = true;
+    [SerializeField, Min(1f)] private float spawnRadius = 50f;
+    [SerializeField, Min(1f)] private float destoryRadius = 60f;
+
+    public float DestroyRadius {get { return destoryRadius; }}
+    
+    private int objectCount = 0;
+
+    public void ChildDestroyed() {
+        this.objectCount -= 1;
+    }
+
+    private void Start() {
+        InitialPopulation();
+    }
+
+    private void Update() {
+        MaintainPopulation(); 
+    }
+
+    private void MaintainPopulation() {
+        if (objectCount < objectLimit) {
+            for (int i = 0; i < spawnInterval; i++) {
+                Vector3 pos = GetRandomPoisiton(false);
+                Spawnable newObj = AddObject(pos);
+                newObj.transform.Rotate(Vector3.forward * Random.Range(-45f, 45f));
+            }
+        }
+    }
+
+    private void InitialPopulation() {
+        for (int i = 0; i < initialPopulation; i++) {
+            Vector3 pos = GetRandomPoisiton(true);
+            Spawnable newObj = AddObject(pos);
+            newObj.transform.Rotate(Vector3.forward * Random.Range(0.0f, 360f));
+        }
+    }
+
+    private Spawnable AddObject(Vector3 position) {
+        GameObject newObject = Instantiate(
+            objectToSpawn.gameObject,
+            position,
+            Quaternion.FromToRotation(Vector3.up, boundingArea.transform.position - position),
+            this.gameObject.transform
+        );
+
+        Spawnable spawnableScript = newObject.GetComponent<Spawnable>();
+        spawnableScript.Spawner = this;
+        spawnableScript.BoundingArea = this.boundingArea;
+        spawnableScript.Velocity = Random.Range(velocityMin, velocityMax); 
+
+        objectCount++;
+        return spawnableScript;
+    }
+
+    private Vector3 GetRandomPoisiton(bool withinCamera) {
+        Vector3 pos = Random.insideUnitCircle;
+        if (withinCamera) {
+            return pos;
+        }
+        pos = pos.normalized;
+        pos *= spawnRadius;
+        pos += boundingArea.transform.position;
+        return pos;
+    }
+
+    private void OnDrawGizmos() {
+        if (showRadiusInEditor) {
+            Color redColor = Color.red;
+            redColor.a = 0.2f;
+            Color greenColor = Color.green;
+            greenColor.a = 0.2f;
+            Gizmos.color = redColor;
+            Gizmos.DrawSphere(boundingArea.transform.position, destoryRadius);
+            Gizmos.color = greenColor;
+            Gizmos.DrawSphere(boundingArea.transform.position, spawnRadius);
+        }
+    }
+}
