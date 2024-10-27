46f25f414dafe692b976c55c1b3c346e2d59deb6
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-10-27 13:26:21 -0400

    always scale the star field to the cameras size

--- PsycheGame/Assets/Scripts/Levels/StarBackgroundController.cs
+++ PsycheGame/Assets/Scripts/Levels/StarBackgroundController.cs
@@ -13,12 +13,17 @@ public class StarBackgroundController : MonoBehaviour {
     // Material in refrence to the star shader attached to the sprite renderer
     // of this component. 
     private Material starMaterial;
+    private SpriteRenderer sRenderer;
+    private Transform camTransform;
 
+    [SerializeField] private Camera cam;
     [SerializeField] private float parallaxStrength = 10.0f;
     [SerializeField] private bool reverseParallaxDirection = false;
-    
+
     private void Start() {
-        this.starMaterial = this.GetComponent<SpriteRenderer>().material; 
+        this.sRenderer = GetComponent<SpriteRenderer>();
+        this.starMaterial = sRenderer.material;
+        this.camTransform = cam.transform;
     }
 
     private void Update() {
@@ -26,6 +31,13 @@ public class StarBackgroundController : MonoBehaviour {
         float brightnessScale = starMaterial.GetFloat(_ptr_brightness_scale);
         starMaterial.SetFloat(_ptr_brightness_scale, brightnessScale + 0.0001f % 10f);
 
+        if (camTransform.hasChanged) {
+            ComputeParallax();
+            ComputeSize();
+        }
+    }
+
+    private void ComputeParallax() {
         // Here is where we actually offset the star material and create
         // the parallax effect
         Vector2 offset = starMaterial.GetVector(_ptr_star_offset);
@@ -33,5 +45,12 @@ public class StarBackgroundController : MonoBehaviour {
         offset.y = this.transform.position.y / this.transform.localScale.y / parallaxStrength;
         offset = reverseParallaxDirection ? offset : -offset;
         starMaterial.SetVector(_ptr_star_offset, offset);
+    }
+
+    private void ComputeSize() {
+        // Scale this background to always fill the camera
+        float worldHeight = cam.orthographicSize * 2f;
+        float worldWidth = worldHeight * cam.aspect;
+        this.transform.localScale = new Vector3(worldWidth, worldHeight, 1f);
     }
 }
