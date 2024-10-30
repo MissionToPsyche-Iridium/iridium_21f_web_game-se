37937cba576a8d8a2ebeffb203f040d29d4eec8e
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-10-30 15:43:23 -0400

    add coroutine for fading from one color to another

--- PsycheGame/Assets/Scripts/ProbeOrbiter/GasBehavior.cs
+++ PsycheGame/Assets/Scripts/ProbeOrbiter/GasBehavior.cs
@@ -1,11 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class GasBehavior : MonoBehaviour {
     private ParticleSystem ps;
     private Material psMaterial;
 
+    [Header("Color")]
+    [SerializeField] private Color startColor = Color.white;
+
     private void Awake() {
         ps = this.GetComponent<ParticleSystem>();
         psMaterial = ps.GetComponent<ParticleSystemRenderer>().sharedMaterial;
+        psMaterial.color = startColor;
     }
-}
+
+    private void Start() {
+        FadeColors(Color.red, 0.5f);   
+    }
+
+    // Fade from one color to another
+    protected void FadeColors(Color endColor, float duration) {
+        Color startColor = psMaterial.color;
+        IEnumerator fadeCoroutine = ColorChange(startColor, endColor, duration);
+        StartCoroutine(fadeCoroutine);
+    }
+
+    private IEnumerator ColorChange(Color startColor, Color endColor, float duration) {
+        float tick = 0f;
+        while (psMaterial.color != endColor) {
+            tick += Time.deltaTime * duration;
+            psMaterial.color = Color.Lerp(startColor, endColor, tick);
+            yield return null;
+        }
+    }
+} 
