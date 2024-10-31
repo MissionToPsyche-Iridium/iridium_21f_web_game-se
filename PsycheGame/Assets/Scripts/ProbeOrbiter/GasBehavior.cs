6dd1df08fa142899e4df4906631c74fd88199d3c
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-10-31 14:31:53 -0400

    prototype more fading effects and using coroutines

--- PsycheGame/Assets/Scripts/ProbeOrbiter/GasBehavior.cs
+++ PsycheGame/Assets/Scripts/ProbeOrbiter/GasBehavior.cs
@@ -5,24 +5,42 @@ public class GasBehavior : MonoBehaviour {
     private ParticleSystem ps;
     private Material psMaterial;
 
-    [Header("Color")]
+    [Header("Start Color")]
     [SerializeField] private Color startColor = Color.white;
 
+    [Header("Color Change On Collision")]
+    [SerializeField] private bool colorChangeCollision = false;
+    [SerializeField] private bool fadeBack = false;
+    [SerializeField] private Color finalColor;
+    [SerializeField] private float changeSpeed = 1.0f;
+    [SerializeField] private float fadeBackWaitTime = 2f;
+
     private void Awake() {
-        ps = this.GetComponent<ParticleSystem>();
-        psMaterial = ps.GetComponent<ParticleSystemRenderer>().sharedMaterial;
-        psMaterial.color = startColor;
+        FetchAndAssingAssets();
+    }
+
+    private void OnValidate() {
+        FetchAndAssingAssets();
     }
 
     private void Start() {
-        FadeColors(Color.red, 0.5f);   
+        //FadeColors(Color.red, 0.5f);   
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (!colorChangeCollision) return;
+        Coroutine fadeRoutine = FadeColors(finalColor, changeSpeed);
+        IEnumerator fadeBackRoutine = ColorChange(finalColor, startColor, changeSpeed);
+        StartCoroutine(
+            WaitDelayRun(fadeRoutine, fadeBackWaitTime, fadeBackRoutine)
+        );
     }
 
     // Fade from one color to another
-    protected void FadeColors(Color endColor, float duration) {
+    protected Coroutine FadeColors(Color endColor, float duration) {
         Color startColor = psMaterial.color;
         IEnumerator fadeCoroutine = ColorChange(startColor, endColor, duration);
-        StartCoroutine(fadeCoroutine);
+        return StartCoroutine(fadeCoroutine);
     }
 
     private IEnumerator ColorChange(Color startColor, Color endColor, float duration) {
@@ -32,5 +50,17 @@ public class GasBehavior : MonoBehaviour {
             psMaterial.color = Color.Lerp(startColor, endColor, tick);
             yield return null;
         }
+    }
+
+    private IEnumerator WaitDelayRun(Coroutine wait, float delay, IEnumerator run) {
+        while (wait != null) { }
+        yield return new WaitForSeconds(delay);
+        yield return StartCoroutine(run);
+    }
+
+    private void FetchAndAssingAssets() {
+        ps = this.GetComponent<ParticleSystem>();
+        psMaterial = ps.GetComponent<ParticleSystemRenderer>().sharedMaterial;
+        psMaterial.color = startColor;
     }
 } 
