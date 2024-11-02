50ec0ff1e9deb4c5debe47164bae925c58e6eef1
Forrest Cranmer <forrest.cranmer@proton.me>
2024-11-02 12:50:22 -0500

    WIP Rebasing

--- PsycheGame/Assets/Scripts/Levels/Asteroid.cs
+++ PsycheGame/Assets/Scripts/Levels/Asteroid.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class Asteroid : Spawnable {
     void Start() { }
     void Update() { }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Boundary"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider);
+        }
+    }
 }
