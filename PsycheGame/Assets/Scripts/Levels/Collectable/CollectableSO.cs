8892164cfe3819c4988c8221aa15d9b23f71469e
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-11-03 11:09:03 -0500

    WIP collectable system with mock hydrogen gas collectable

---
+++ PsycheGame/Assets/Scripts/Levels/Collectable/CollectableSO.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public abstract class CollectableSO : ScriptableObject {
+    public abstract void Collect(GameObject objectCollected);
+}
---
+++ PsycheGame/Assets/Scripts/Levels/Collectable/HydrogenGas.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Collectable/Hydrogen Gas", fileName = "HydrogenGas")]
+public class HydrogenGas : CollectableSO {
+    public override void Collect(GameObject objectCollected) {
+    }
+}
