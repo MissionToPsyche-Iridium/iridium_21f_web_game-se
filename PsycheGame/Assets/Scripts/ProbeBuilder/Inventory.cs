2132a5c7a8f48e4d139e924124f61044b6feba62
hkmargolis <hmargol1@asu.edu>
2024-11-04 12:25:52 -0800

    Updates inventory and probe component classes

--- PsycheGame/Assets/Scripts/ProbeBuilder/Inventory.cs
+++ PsycheGame/Assets/Scripts/ProbeBuilder/Inventory.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class InventoryContainer<T>
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<ProbeComponent> _container;
+
+    public InventoryContainer()
     {
-        
+        _container = new List<ProbeComponent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public ProbeComponent GetItem(string id)
     {
-        
+        foreach (ProbeComponent item in _container)
+        {
+            if (item.getId().Equals(id))
+            {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public int GetItemQuantity(string id)
+    {
+        foreach (ProbeComponent item in _container)
+        {
+            if (item.getId().Equals(id))
+            {
+                return item.getQuantity();
+            }
+        }
+        return 0;
+    }
+
+    public List<int> GetItemIds()
+    {
+        List<int> ids = new List<int>();
+        foreach (ProbeComponent item in _container)
+        {
+            ids.Add(item.getId());
+        }
+        return ids;
+    }
+
+    public void AddItem(ProbeComponent probeComponent)
+    {
+        _container.Add(probeComponent);
+    }
+
+    public void UpdateItemQuantity(string id, int quantity)
+    {
+        foreach (ProbeComponent item in _container)
+        {
+            if (item.getId().Equals(id))
+            {
+                item.setQuantity(quantity);
+                break;
+            }
+        }
+    }
+
+    public void RemoveItem(ProbeComponent probeComponent)
+    {
+        for (int index = 0; index < _container.Count; index++)
+        {
+            if (_container[index].getId().Equals(probeComponent.getId()))
+            {
+                _container.RemoveAt(index);
+                break;
+            }
+        }
     }
 }
--- PsycheGame/Assets/Scripts/ProbeBuilder/ProbeComponent.cs
+++ PsycheGame/Assets/Scripts/ProbeBuilder/ProbeComponent.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 
 public class ProbeComponent
@@ -7,12 +8,18 @@ public class ProbeComponent
     private string name;
     private string description;
     private Sprite sprite;
+    private int id;
+    private int quantity;
+    private readonly int max_quantity = 10;
+
 
     public ProbeComponent(string name, string description, Sprite sprite)
     {
         this.name = name;
         this.description = description;
         this.sprite = sprite;
+        this.id = sprite.GetInstanceID();
+        this.quantity = 0;
     }
 
     public string GetName()
@@ -30,8 +37,29 @@ public class ProbeComponent
         return sprite;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+
+    public void incrementQuantity() {
+        this.quantity++;
+    }
+
+    public void decrementQuantity() {
+        this.quantity--;
+    }
+
     public ProbeComponent Clone()
     {
+        //TODO creat new id (get instance id?)
         return new ProbeComponent(name, description, sprite);
     }
 }
