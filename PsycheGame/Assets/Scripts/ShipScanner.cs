4aad62f8deb60041ad1a75feaf1a42840b431e12
Forrest Cranmer <forrest.cranmer@proton.me>
2024-11-05 17:01:46 -0600

    Base implementation of scanning. Need to wire up scanning logic on press once animation is complete.

---
+++ PsycheGame/Assets/Scripts/ShipScanner.cs
@@ -0,0 +1,21 @@
+// using UnityEngine;
+
+// public class ShipScanner : MonoBehaviour
+// {
+//     private Animator animator;
+//     private bool isScanning = false;
+
+//     void Start()
+//     {
+//         animator = GetComponent<Animator>();
+//     }
+
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.F))
+//         {
+//             isScanning = !isScanning;  
+//             animator.SetBool("IsScanning", isScanning);
+//         }
+//     }
+//
