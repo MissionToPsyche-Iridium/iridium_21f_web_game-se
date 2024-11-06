8b2317e2f7d47d3edf5a1df31bc568b190d99b66
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-11-06 12:20:06 -0500

    refactor fuel bar logic into standalone class

--- PsycheGame/Assets/Scripts/Levels/ShipMovement.cs
+++ PsycheGame/Assets/Scripts/Levels/ShipMovement.cs
@@ -8,11 +8,6 @@ using UnityEngine.UI;
 public class ShipMovement : MonoBehaviour {
     [SerializeField] private bool fuelEnabled = true;
 
-    [SerializeField] private GameObject fuelBarColor;
-    [SerializeField] private Slider fuelBar;
-    [SerializeField] protected GameObject fuelBarPanel;
-
-    private float fuelLowThreshold = 20f;
     public float moveSpeed = 5f; 
     public float fuelConsumptionRate = 1f;
 
@@ -20,8 +15,6 @@ public class ShipMovement : MonoBehaviour {
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
-        fuelBarColor = GameObject.Find("FuelBarFill");
-        fuelBarPanel = GameObject.Find("HUD Panel");
     }
 
     void Update() {
@@ -37,7 +30,6 @@ public class ShipMovement : MonoBehaviour {
             ShipManager.Fuel = Mathf.Max(ShipManager.Fuel, 0f);
             rb.velocity = movement * moveSpeed;
             RotateShip(movement);
-            UpdateFuelDisplay();
         }
         else if (fuelEnabled)
         {
@@ -50,31 +42,4 @@ public class ShipMovement : MonoBehaviour {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         rb.rotation = angle - 90f;
     }
-
-    
-    private void UpdateFuelDisplay() {
-        float fuel = ShipManager.Fuel;
-        fuelBar.value = fuel;
-
-        if (fuel < 25f) {
-            fuelBarColor.GetComponent<Image>().color = Color.red;
-            StartCoroutine(FlashLowFuel());
-        } else if (fuel < 50f) { 
-            fuelBarColor.GetComponent<Image>().color = Color.yellow;
-        } else {
-            fuelBarColor.GetComponent<Image>().color = Color.green;
-        }
-    }
-
-    IEnumerator FlashLowFuel()
-    {
-        while (ShipManager.Fuel < fuelLowThreshold)
-        {
-            fuelBarPanel.GetComponent<Image>().color = Color.red;
-            yield return new WaitForSeconds(0.5f);
-            fuelBarPanel.GetComponent<Image>().color = Color.white;
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
 }
-
