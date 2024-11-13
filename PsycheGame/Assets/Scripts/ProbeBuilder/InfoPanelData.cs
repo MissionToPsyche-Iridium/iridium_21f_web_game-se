5192747a3ddc47b4e2d83654756c716e044ee433
hkmargolis <hmargol1@asu.edu>
2024-11-13 13:23:52 -0800

    Creates InfoPanelData class and UI InfoPanel

--- PsycheGame/Assets/Scripts/ProbeBuilder/InfoPanelData.cs
+++ PsycheGame/Assets/Scripts/ProbeBuilder/InfoPanelData.cs
@@ -6,6 +6,7 @@ using System.Xml.Serialization;
 using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
@@ -14,8 +15,9 @@ public class InfoPanelData: MonoBehaviour
 {
     public TextAsset data;
     public PartsList parts = new PartsList();
-    //public SerializeField Image[] images;
-    Canvas parent;
+    public Sprite[] spritesArr;
+    
+    //public GameObject currentObject; //TODO get most recent sprite that has been clicked
 
     [System.Serializable]
     public class Part {
@@ -27,15 +29,30 @@ public class InfoPanelData: MonoBehaviour
     [System.Serializable]
     public class PartsList {
         public Part[] part;
-         //Image image;
     }
 
+    [System.Serializable] 
+    public class Images{
+        public Sprite[] sprites;
+    }
     public void Start() {
         parts = JsonUtility.FromJson<PartsList>(data.text);
-        GetComponentInChildren<TMP_Text>().text = parts.part[0].name;
-
+        
+        GameObject.Find("PartName").GetComponentInChildren<TMP_Text>().text = parts.part[0].name;;
+        GameObject.Find("PartDescription").GetComponentInChildren<TMP_Text>().text = parts.part[0].description;
+        GameObject.Find("PartImage").GetComponentInChildren<UnityEngine.UI.Image>().sprite = spritesArr[0];
     }
 
-    
+    // void Update () {
+
+    //     if (Input.GetMouseButtonDown(0) )
+    //     {
+           
+    //          currentObject = UnityEngine.EventSystems.EventSystem.current.gameObject;
+    //         //currentObject.GetComponent<Sprite>();
+    //         Debug.Log("current sprite " + currentObject.name);   
+    //     }
+
+    // }
 
 }
