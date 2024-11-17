e8f84c9b6e58b4ac11c9ea8da4798c80916df985
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-11-16 20:21:45 -0500

    WIP fix issue causing progress bar to not render correctly

--- PsycheGame/Assets/Scripts/Levels/UI/ProgressBarUI.cs
+++ PsycheGame/Assets/Scripts/Levels/UI/ProgressBarUI.cs
@@ -4,14 +4,16 @@ using UnityEngine.UI;
 public class ProgressBarUI : MonoBehaviour {
     [SerializeField] private Image fill;
     [SerializeField] private Image mask;
-    [SerializeField] private Color color;
 
     public Progress progress;
 
-    void Update() {
+    private void Awake() {
+        mask.fillAmount = 0; 
+    }
+
+    private void Update() {
         if (progress != null) {
             float fillAmount = progress.Value / Progress.COMPLETE_PROGRESS;        
-            fill.color = color;
             mask.fillAmount = fillAmount;
         }
     }
