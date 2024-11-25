f07859c553b2b0a60e89c4defde642392ff0f14c
U-BIGSAL\Hansen <thanse23@asu.edu>
2024-11-25 17:34:48 -0500

    add base logic for displaying line between tether and tether point

--- PsycheGame/Assets/Scripts/Levels/Ship/ShipTether.cs
+++ PsycheGame/Assets/Scripts/Levels/Ship/ShipTether.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 [RequireComponent(typeof(DistanceJoint2D))]
 public class ShipTether : MonoBehaviour {
     [SerializeField] private Camera cameraRef;
@@ -8,8 +9,12 @@ public class ShipTether : MonoBehaviour {
 
     private Vector3 tetherPoint;
     private DistanceJoint2D distJoint;
+    private LineRenderer lRenderer;
 
     private void Start() {
+        lRenderer = this.GetComponent<LineRenderer>();
+        lRenderer.enabled = false;
+
         distJoint = this.GetComponent<DistanceJoint2D>(); 
         distJoint.enabled = false;
         distJoint.enableCollision = true;
@@ -29,16 +34,25 @@ public class ShipTether : MonoBehaviour {
             if (hit.collider != null) {
                 tetherPoint = hit.point;
                 tetherPoint.z = 0;
+
                 distJoint.connectedAnchor = tetherPoint;
                 distJoint.enabled = true;
                 distJoint.distance = tetherLength;
+
+                lRenderer.enabled = true;
+                lRenderer.SetPosition(0, tetherPoint);
+                lRenderer.SetPosition(1, transform.position);
             }
 
         }
 
+        if (lRenderer.enabled) { 
+            lRenderer.SetPosition(1, transform.position);
+        }
+
         if (Input.GetMouseButtonUp(0)) {
+            lRenderer.enabled = false;
             distJoint.enabled = false;
         }
     }
-
 }
