61a68769a618cd0ea19b4a5661df1bb9ba540ac6
hkmargolis <hmargol1@asu.edu>
2024-11-27 14:04:45 -0800

    Adds new assets to inventory

---
+++ PsycheGame/Assets/Scripts/ProbeDesignBrowser/ProbeDesign.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbeDesign : MonoBehaviour
+{
+    private Sprite sprite; //flattened image of design
+    private String name; //name of design
+    private String json; //contains the probe parts attached (names and locations) these are saved in the container game data class
+
+   
+}
