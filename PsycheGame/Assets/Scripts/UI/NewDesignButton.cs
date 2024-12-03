8cbd4995801ee0685d62675949f42b035ee02643
hkmargolis <hmargol1@asu.edu>
2024-12-03 10:25:57 -0800

    Adds buttons to navigate between scenes

---
+++ PsycheGame/Assets/Scripts/UI/NewDesignButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
+
+public class NewDesignButton : MonoBehaviour, IPointerDownHandler
+{
+   private AudioClip _swooshSound;
+
+    private void Awake()
+    {
+        _swooshSound = Resources.Load<AudioClip>("Audio/laser-swoosh");
+        this.AddComponent<AudioSource>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("ProbeBuilder");
+        GetComponent<AudioSource>().PlayOneShot(_swooshSound, 1.0f);
+    }
+}
