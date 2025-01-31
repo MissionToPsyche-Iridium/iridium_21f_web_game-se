cdae2398fe289fd397f43977935649672387d4f4
Shawn Chen <115514754+schen337-asu@users.noreply.github.com>
2025-01-30 20:24:08 -0800

    added filler bars as part of the attribute bar UI design

---
+++ PsycheGame/Assets/AttributeTracker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    AttributeTracker.cs
+    Description: this script is responsible for tracking the attributes of the probe components.  
+    
+    version: 1.0 
+
+    This script updates the child components' attributes based on the current probe component's attributes using the call to the
+    UpdateChildAttributes() method.
+*/
+
+public class AttributeTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
