b36f0d99b1df62a36a3d6afb6fb574ede1f6c08c
schen337-asu <115514754+schen337-asu@users.noreply.github.com>
2025-02-17 17:21:05 -0800

    remove unncessary using/import statements

--- PsycheGame/Assets/Scripts/ProbeBuilder/InfoPanelData.cs
+++ PsycheGame/Assets/Scripts/ProbeBuilder/InfoPanelData.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
-using Microsoft.Unity.VisualStudio.Editor;
 using TMPro;
 using Unity.Collections;
 using Unity.VisualScripting;
