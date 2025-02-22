06f8414213b125ca197329f3dc84efc27317d514
U-BIGSAL\Hansen <thanse23@asu.edu>
2025-02-22 12:20:30 -0500

    hotfix for leaderboard display issue preventing game from compiling

--- PsycheGame/Assets/Scripts/Levels/LevelConfigs/LevelManager.cs
+++ PsycheGame/Assets/Scripts/Levels/LevelConfigs/LevelManager.cs
@@ -136,7 +136,8 @@ private IEnumerator LoadLevelAsync(int levelIndex, bool success)
     {
         SetLoadingState(true);
         if(success){
-            loadingText.text = $"You scored: {playerScore}\n {leaderBoard.DisplayLeaderboard} \nLoading Level {levelIndex + 1}...";
+            loadingText.text = $"You scored: {playerScore}\nLoading Level {levelIndex + 1}...";
+            leaderBoard.DisplayLeaderboard();
         } else {
             loadingText.text = $"Better luck this time! Loading Level {levelIndex}";
         }
