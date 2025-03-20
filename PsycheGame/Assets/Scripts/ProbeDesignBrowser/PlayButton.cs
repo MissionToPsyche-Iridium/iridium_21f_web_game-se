7e3e47ba114e7ae17f0fe38b7a043694967a1a53
hkmargolis <hmargol1@asu.edu>
2025-03-20 14:03:01 -0700

    US#347 Adds probe silhouette sprites

--- PsycheGame/Assets/Scripts/ProbeDesignBrowser/PlayButton.cs
+++ PsycheGame/Assets/Scripts/ProbeDesignBrowser/PlayButton.cs
@@ -6,16 +6,18 @@ using Unity.VisualScripting;
 public class PlayButton : MonoBehaviour, IPointerClickHandler
 {
    private AudioClip _swooshSound;
+    private AudioSource _audioSource;
+
 
     private void Awake()
     {
-        _swooshSound = Resources.Load<AudioClip>("Audio/laser-swoosh");
-        this.AddComponent<AudioSource>();
+         _swooshSound = Resources.Load<AudioClip>("Audio/laser-swoosh");
+        _audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         SceneManager.LoadScene("ExplorationLevel");
-        GetComponent<AudioSource>().PlayOneShot(_swooshSound, 1.0f);
+        _audioSource.PlayOneShot(_swooshSound, 1.0f);
     }
 }
